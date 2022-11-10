82c0a8efc050de25e2bc6b2a43718da9a0974e6f
LeomarisReyes <reyes.leomaris@gmail.com>
2022-11-10 11:55:58 -0400

    Grid in .NET MAUI

---
+++ 3.Grid/PagePlatziSample/Views/Pages/FlyoutSample/FlyoutPageItem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace PagePlatziSample.Views.Pages.FlyoutSample
+{
+    public class FlyoutPageItem
+    {
+        public string Title { get; set; }
+        public string IconSource { get; set; }
+        public Type TargetType { get; set; }
+    }
+}
+
